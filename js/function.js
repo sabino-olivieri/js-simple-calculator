e5c3e5a0f0f9a2e6d58cdcbdeacc3d09b4d7246b
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-04-08 18:43:57 +0200

    feat: function for operation

--- js/function.js
+++ js/function.js
@@ -1,3 +1,8 @@
+/**
+ * Description aggiungo il valore del bottone clicatto al display
+ * @param {object} elemClicked elemento bottone cliccato
+ * @param {object} text elemento dove aggiungere valori
+ */
 function addNumber(elemClicked, text) {
 
     const textValue =  text.innerHTML;
@@ -6,9 +11,44 @@ function addNumber(elemClicked, text) {
     const clickedValue = elemClicked.value;
     console.log("Tasto:", clickedValue);
 
-    if(textValue != "0") {
+    if(textValue != "0" && result === "none") {
         text.innerHTML += clickedValue;
     } else {
         text.innerHTML = clickedValue;
+        result = "none";
     }
+
+    return result;
+}
+
+function equalOperation (firstNum, secondNum, operation) {
+    
+    let res = 0;
+
+    switch (operation) {
+        case "+":
+            res = firstNum + secondNum;
+            
+            break;
+    
+        case "-":
+            res = firstNum - secondNum;
+
+            break;
+
+        case "*":
+            res = firstNum * secondNum;
+
+            break;
+
+        case "/":
+            res = firstNum / secondNum;
+
+            break;
+
+        default:
+            break;
+    }
+
+    return res;
 }
--- js/script.js
+++ js/script.js
@@ -2,12 +2,83 @@
 const numElem = document.querySelectorAll(".number");
 console.log(numElem);
 
+// prendo elementi per operazioni
+const operatorElem = document.querySelectorAll(".operations");
+console.log(operatorElem);
+
+// prendo elemento uguale
+const equalElem = document.querySelector(".result");
+
+// prendo elemento canc
+const cancElem = document.querySelector(".canc");
+
+
+let num1 = 0;
+let num2 = 0;
+let operator ="";
+let result = "none";
+
 // prendo elemento scritta dispaly
 const dispalyElem = document.querySelector(".text-display");
 
-//aggiungo event listner ai numeri
+// aggiungo event listner ai numeri
 for (let i = 0; i < numElem.length; i++) {
+
     numElem[i].addEventListener("click", function () {
-        addNumber(numElem[i], dispalyElem)
+
+        const flag = addNumber(numElem[i], dispalyElem, result);
+        result = flag;
+
     });
 }
+
+// aggiungo event listner agli operatori
+for(let i = 0; i < operatorElem.length; i++) {
+    operatorElem[i].addEventListener("click", function(){
+
+        operator = operatorElem[i].value;
+        console.log(operator);
+
+        if (num1 === 0) {
+
+            num1 = parseFloat(dispalyElem.innerHTML);
+            console.log(num1, "Numero1");
+
+        } else {
+
+            num2 = parseFloat(dispalyElem.innerHTML);
+            console.log("numero2", num2);
+            num1 = equalOperation(num1, num2, operator);
+
+        }
+
+        dispalyElem.innerHTML = "0";
+    });
+}
+
+// aggiungo event listner a uguale
+equalElem.addEventListener("click", function() {
+    num2 = parseFloat(dispalyElem.innerHTML);
+    console.log(num2, typeof num2);
+    result = equalOperation(num1, num2, operator);
+    console.log(result);
+    
+
+    if (result === Infinity) {
+        result = "Errore"
+    }
+
+    num1 = 0;
+    num2 = 0;   
+
+    dispalyElem.innerHTML = result;
+}); 
+
+// aggiungo event listner a canc
+cancElem.addEventListener("click", function() {
+    num1 = 0;
+    num2 = 0;
+    operator = "";
+    result = "none";
+    dispalyElem.innerHTML = "0";
+});
